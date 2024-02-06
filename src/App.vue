3a1a2be02a3d6ae193c9bb5599b88458a0c3d32b
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-06 15:37:40 +0100

    add mounted axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <script>
+//import axios from 'axios'; //questa la sta importando de la libreria che ho scaricato di axios e messo nell header
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      Emails: []
+    }
+  },
+
+  mounted() {
+    for (let index = 0; index < 10; index++) {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          console.log(response);
+          console.log(this);
+          this.Emails.push(response.data.response);
+
+        })
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <h3>Lista Emails:</h3>
+  <ul v-for="email in Emails">
+    <li>{{ email }}</li>
+  </ul>
 </template>
 
 <style></style>
