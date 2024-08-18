cc7b5b3cb96912ee0b46c3cdb661c9b831241e85
scarlett-troncoso <scar.labussieri@gmail.com>
2024-08-18 14:19:51 +0200

    add base for branch github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/vue-email-list/' : '/', // questo é stato aggiunto per utilizzare GitHub Pages
 })
